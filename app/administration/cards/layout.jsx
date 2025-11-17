bccd6125a108005e6230bca2cee30a522c7ddcff
diegomurillocorrea <diegomurillocorrea@gmail.com>
2025-11-16 20:47:28 -0600

    ✅ Finish the Cards view

---
+++ app/administration/cards/layout.jsx
@@ -0,0 +1,11 @@
+export const metadata = {
+    title: "Cards | Streaming Murillo App",
+};
+
+export default function CardsLayout({ children }) {
+    return (
+        <>
+            {children}
+        </>
+    );
+}
